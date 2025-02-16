d08834724cd8129909d7388db353a2e93fd33f20
MFaisal077 <mohammadfaisal87897@gmail.com>
2025-02-16 12:48:39 +0000

    Changed the look and feel of the website

--- javascript.js
+++ javascript.js
@@ -1,1 +1,11 @@
-console.log(a+b);
+// Add to javascript.js
+document.addEventListener('DOMContentLoaded', function() {
+    const currentPage = window.location.pathname.split('/').pop();
+    const navLinks = document.querySelectorAll('.Navigation_bar a');
+    
+    navLinks.forEach(link => {
+        if (link.getAttribute('href') === currentPage) {
+            link.classList.add('active');
+        }
+    });
+});
